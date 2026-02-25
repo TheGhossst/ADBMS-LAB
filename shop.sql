53d0092821ec363667887013211210bc421f059e
Nandu Krishna M <116820865+TheGhossst@users.noreply.github.com>
2026-02-25 16:11:00 +0530

    Update shop.sql

--- shop.sql
+++ shop.sql
@@ -1,2 +1,32 @@
-CREATE TABLE CUSTOMER(
+CREATE TABLE cust (
+    id NUMBER PRIMARY KEY,
+    name VARCHAR2(50),
+    age NUMBER,
+    gender VARCHAR2(10),
+    profession VARCHAR2(50)
+);
+
+CREATE TABLE shop_details (
+    shop_name VARCHAR2(50) PRIMARY KEY,
+    area VARCHAR2(50),
+    place VARCHAR2(50)
+);
+
+CREATE TABLE items (
+    i_name VARCHAR2(50) PRIMARY KEY,
+    brand VARCHAR2(50),
+    cost NUMBER
+);
+
+CREATE TABLE transactions (
+    tid NUMBER,
+    purchase_date DATE,
+    id NUMBER,
+    shop_name VARCHAR2(50),
+    i_name VARCHAR2(50),
+    PRIMARY KEY (id, shop_name, i_name),
+    FOREIGN KEY (id) REFERENCES cust(id),
+    FOREIGN KEY (shop_name) REFERENCES shop_details(shop_name),
+    FOREIGN KEY (i_name) REFERENCES items(i_name)
+)
   
